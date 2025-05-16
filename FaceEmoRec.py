9fd8801cd5a76ba0483eb3f38bb3f3355535d857
PariveshNegi <139899836+PariveshNegi@users.noreply.github.com>
2025-05-16 19:41:13 +0530

    Update FaceEmoRec.py

--- FaceEmoRec.py
+++ FaceEmoRec.py
@@ -1,79 +1,128 @@
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+!pip install keras_preprocessing
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras_preprocessing.image import load_img
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
+import os
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.utils import to_categorical
+!unzip /content/drive/MyDrive/ColabNotebooks/images.zip
+
+TRAIN_DIR = "images/train"
+TEST_DIR = "images/test"
+
+def create_dataframe(dir):
+    image_paths = []
+    labels = []
+    for lable in os.listdir(dir):
+        for imagename in os.listdir(os.path.join(dir,lable)):
+            image_paths.append(os.path.join(dir,lable,imagename))
+            labels.append(lable)
+        print(lable,"completed")
+    return image_paths,labels
+
+train=pd.DataFrame()
+train['image'],train['label']=create_dataframe(TRAIN_DIR)
+
+test=pd.DataFrame()
+test['image'],test['label']=create_dataframe(TEST_DIR)
+
+from tqdm.notebook import tqdm
+def extract_features(images):
+    features = []
+    for image in tqdm(images):
+        img=load_img(image,grayscale=True)
+        img=np.array(img)
+        features.append(img)
+    features=np.array(features)
+    features=features.reshape(len(features),48,48,1)
+    return features
+
+train_features=extract_features(train['image'])
+
+test_features = extract_features(test['image'])
+
+x_train=train_features/255.0
+x_test=test_features/255.0
+
 from sklearn.preprocessing import LabelEncoder
 
-# Load and encode labels
-le = LabelEncoder()
+le=LabelEncoder()
 le.fit(train['label'])
-y_train = le.transform(train['label'])
+
+y_train =  le.transform(train['label'])
 y_test = le.transform(test['label'])
 
-y_train = to_categorical(y_train, num_classes=7)
-y_test = to_categorical(y_test, num_classes=7)
+y_train=to_categorical(y_train,num_classes=7)
+y_test=to_categorical(y_test,num_classes=7)
 
-# Define CNN model
-model = Sequential()
-model.add(Conv2D(128, kernel_size=(3,3), activation='relu', input_shape=(48,48,1)))
+model=Sequential()
+model.add(Conv2D(128,kernel_size=(3,3),activation='relu',input_shape=(48,48,1)))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.4))
 
-model.add(Conv2D(256, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(256,kernel_size=(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.4))
 
-model.add(Conv2D(512, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(512,kernel_size=(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.4))
 
-model.add(Conv2D(512, kernel_size=(3,3), activation='relu'))
+model.add(Conv2D(512,kernel_size=(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.4))
 
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
+model.add(Dense(512,activation='relu'))
 model.add(Dropout(0.4))
-model.add(Dense(256, activation='relu'))
+model.add(Dense(256,activation='relu'))
 model.add(Dropout(0.3))
-model.add(Dense(7, activation='softmax'))
+model.add(Dense(7,activation='softmax'))
 
-# Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 
-# Train the model
-model.fit(x=x_train, y=y_train, batch_size=128, epochs=100, validation_data=(x_test, y_test))
+model.fit(x=x_train,y=y_train,batch_size=128,epochs=100,validation_data=(x_test,y_test))
 
-# Save the model
-model_json = model.to_json()
+mode_json= model.to_json()
 with open("emotiondetection.json", "w") as json_file:
-    json_file.write(model_json)
+    json_file.write(mode_json)
 model.save("emotiondetection.h5")
 
-# Load the model
-from tensorflow.keras.models import model_from_json
-json_file = open("emotiondetection.json", "r")
-model_json = json_file.read()
+from keras.models import model_from_json
+
+json_file=open("emotiondetection.json","r")
+model_json=json_file.read()
 json_file.close()
-model = model_from_json(model_json)
+model=model_from_json(model_json)
 model.load_weights("emotiondetection.h5")
 
-# Label mapping
 label = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 
-def preprocess_image(image):
-    img = load_img(image, color_mode='grayscale', target_size=(48,48))
-    feature = np.array(img)
-    feature = feature.reshape(1,48,48,1)
-    return feature / 255.0
+def ef(image):
+    img=load_img(image,grayscale=True)
+    feature=np.array(img)
+    feature =feature.reshape(1,48,48,1)
+    return feature/255.0
 
-# Test the model
-image = 'images/train/happy/82 copy.jpg'
-print("Original:", image)
-img = preprocess_image(image)
-pred = model.predict(img)
-pred_label = label[np.argmax(pred)]
-print("Predicted emotion:", pred_label)
+image ='images/test/surprise/10306.jpg'
+print("original",image)
+img =ef(image)
+pred=model.predict(img)
+pred_label = label[pred.argmax()]
+print("predicted is ",pred_label)
+
+import numpy as np
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+y_pred = model.predict(x_test)
+y_pred_classes = np.argmax(y_pred, axis=1)
+y_true_classes = np.argmax(y_test, axis=1)
+
+print("Classification Report:\n", classification_report(y_true_classes, y_pred_classes))
+print("Accuracy:\n", accuracy_score(y_true_classes, y_pred_classes))
+print("Confusion Matrix:\n", confusion_matrix(y_true_classes, y_pred_classes))
